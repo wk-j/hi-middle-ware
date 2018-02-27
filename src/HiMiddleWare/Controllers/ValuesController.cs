357c7717fd00f0c4ccedf367e8585c8a3df3d1b6
wk-j <somnuk.wk@gmail.com>
2018-02-28 01:08:13 +0700

    Initial commit

---
+++ src/HiMiddleWare/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiMiddleWare.Controllers {
+    [Route("api/[controller]/[action]")]
+    public class ValuesController : Controller {
+        [HttpGet]
+        public string Hi() {
+            return "Hi";
+        }
+    }
+}
---
+++ src/HiMiddleWare/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HiMiddleWare {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            app.Use(async (context, next) => {
+                var ok = context.Request.Headers.TryGetValue("Custom-Token", out var customToken);
+                if (ok) {
+                    Console.WriteLine("Next ...");
+                    await next.Invoke();
+                } else {
+                    context.Response.Headers.Add("Hi", "Hi");
+                    context.Response.StatusCode = StatusCodes.Status511NetworkAuthenticationRequired;
+                    await context.Response.WriteAsync("Invalid token!");
+                }
+            });
+
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
